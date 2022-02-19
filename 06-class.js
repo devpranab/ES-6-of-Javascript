6d88aaf702de9c17babd43e79c2258dfff3e1bfa
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-19 21:25:50 +0530

    Add files via upload
    class

---
+++ 06-class.js
@@ -0,0 +1,35 @@
+/*
+//before used
+//constructor
+function Person(age,name){
+    this.age = age;
+    this.name = name;
+}
+//add method
+Person.prototype.getName = function(){
+    console.log(this.name);
+}
+*/
+
+//class - es-6 - part of oop
+
+/*
+//now uses
+class Student {
+    constructor(Sid, Sname){
+        this.id = Sid;
+        this.name = Sname;
+    }
+    //add method
+    getName(){
+        console.log(this.name);
+    }
+
+}
+
+const student1 = new Student(1, "suvo");
+console.log(student1);
+console.log(student1.id);
+const student2 = new Student(2, "mahi");
+console.log(student2);
+*/

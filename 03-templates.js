c5efb4d808ea9370a67d46b7dd8e3ae989e1f2bd
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-19 21:05:21 +0530

    Add files via upload
    templates literals

---
+++ 03-templates.js
@@ -0,0 +1,30 @@
+//template literals - es-6
+
+/*
+//before comming es-6
+const firstName = "pranab";
+const lastName = "sarkar";
+const concat = firstName + " " + lastName;
+console.log(concat);
+
+
+const multiline = "i love you \n"
+                   + "i miss you \n"
+                   + "i need you";
+console.log(multiline);
+*/
+
+/*
+//after came es-6 - template literals
+const firstName = "pranab";
+const lastName = "sarkar";
+//const concat = `${firstName} ${lastName}`;
+const concat = `${firstName} ${lastName} ${10 + 4}`;
+console.log(concat);
+
+const multiline = `i love you
+i miss you 
+i need you`;
+               
+console.log(multiline);
+*/

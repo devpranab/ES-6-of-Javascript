4885fb3bc4c0776f2ee9c6f82967da22c004ad4f
Pranab Sarkar <86111675+devpranab@users.noreply.github.com>
2022-02-19 21:32:59 +0530

    Add files via upload
    set

---
+++ 12-set.js
@@ -0,0 +1,60 @@
+//set - data structure
+
+//declare new set
+let mySet = new Set([1,2,3,3,4,5]);
+// console.log(mySet);
+
+//set
+// mySet.add("hi");
+// mySet.add(1); // no add
+
+//clear
+// mySet.clear();
+
+//delete
+// mySet.delete(5);
+
+//has
+// let has2 = mySet.has(2);
+// console.log(has2);
+
+//size
+// console.log(mySet.size);
+
+//iterating
+// for(let x of mySet){
+// console.log(x);
+// }
+
+// for(let x of mySet.entries()){
+// console.log(x);
+// }
+
+// for(let [x] of mySet.entries()){
+// console.log(x);
+// }
+
+// for(let x of mySet.values()){
+// console.log(x);
+// }
+
+
+//entries
+// let iter = mySet.entries();
+// console.log(iter);
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+// console.log(iter.next());
+
+
+//spread operator
+// let iter = [...mySet.entries()];
+// let iter = [...mySet.keys()];
+// let iter = [...mySet.values()];
+// console.log(iter);
+
+
+// let iter = [...mySet.values()];
+// console.log(iter);
+// mySet.forEach(value => console.log(value));
